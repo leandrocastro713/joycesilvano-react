8d9f7735ab5f51c59a00e0b594c5cee81405273d
leandro castro <leandro.713@hotmail.com>
2023-06-09 18:09:14 -0300

    menu factoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from "./Header/Header"
+import Menu from "./Menu/Menu"
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   );
 }
---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import "./Menu.css"
+export default function Menu(){
+  return(
+    <nav id="barra-de-menu">
+        <ul id="menu" type = 'none'>
+            <li><a onClick='inicio-container-show' href="#">Início</a></li>
+            <li><a onClick='saude-container-show' href="#">Saúde</a></li>
+            <li><a onClick='produtos-container-show'href="#">Produtos</a></li>
+            <li><a onClick='dicas-container-show'href="">Dicas</a></li>
+            <li><a onClick='galeria-container-show' href="">Galeria</a></li>
+            <li><a onClick='contato-container-show' href="">Contato</a></li>
+        </ul>
+    </nav>
+  )
+}
