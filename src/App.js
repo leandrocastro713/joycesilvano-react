d4d9a7410b2a8b5dd1e438acdac6cba977f193cc
leandro castro <leandro.713@hotmail.com>
2023-06-19 20:19:35 -0300

    rodape

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Header/Header"
 import Menu from "./Menu/Menu"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import InicioPage from './InicioPage/InicioPage'
+import Rodape from './Rodape/Rodape'
 
 export default function App() {
+  
   const[defaultPage, setDefaultPage] = useState('inicio')
   const[showInicioPage, setShowInicioPage] = useState(true)
   const[showSaudePage, setShowSaudePage] = useState(false)
@@ -12,11 +14,19 @@ export default function App() {
   const[showProdutosPage, setShowProdutosPage] = useState(false)
   const[showGaleriaPage, setShowGaleriaPage] = useState(false)
   const[showContatoPage, setShowContatoPage] = useState(false)
+  
+  useEffect(()=>{
+
+  })
+
   return (
+  
     <div className="App">
       <Header />
       <Menu />
       {showInicioPage && <InicioPage />}
+      <Rodape />
     </div>
+  
   );
 }
---
+++ src/Rodape/Rodape.jsx
@@ -0,0 +1,41 @@
+import "./Rodape.css"
+
+export default function Rodape() {
+  return (
+    <footer className="rodape">
+      <div className="left-footer">
+        <p className="lf-text">
+          Joyce Silvano Cabelos Livres
+          <br />
+        </p>
+        <div className="wp-footer">
+          <img src={require("../imagens/logo-whatsapp.png")} alt="" />
+          <p>(51) 99849-7549</p>
+        </div>
+        <div className="ig-footer">
+          <img src={require("../imagens/logo-instagram.png")} alt="" />
+          <p>joycesilvanocabeloslivres</p>
+        </div>
+        <div className="fb-footer">
+          <img src={require("../imagens/logo-facebook.png")} alt="" />
+          <p>joycesilvanocabeloslivres</p>
+        </div>
+        <div className="lf-text">
+          <img id="local-icon" src={require("../imagens/local.png")} alt="" />
+          <a href="https://goo.gl/maps/Zr7eW77xYXTCKorJ9" target="_blank">
+            <p>Avenida Senador Salgado Filho, 8569</p>
+            <p>Viamão - RS</p>
+          </a>
+        </div>
+      </div>
+
+      <div className="right-footer">
+        <p className="rf-text">
+          "Site produzido por <a href="https://portifolio2-eight.vercel.app/" target="_blank">
+            <strong>@urzzomidia</strong>
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
